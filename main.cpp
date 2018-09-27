90921aa47143e888c44b93389e942bc433a5d578
asolovev99 <32899295+asolovev99@users.noreply.github.com>
2018-09-27 12:18:28 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,53 +1,74 @@
 #include <iostream>
+using namespace std;
 class String
 {
     char *data;
     int lenght;
     public:
         String (){
-            data = new char[1];
-            data[0] ='\0';
+            data = new char;
+            *data ='\0';
             lenght = 0;
+		//	printf("made empty\n");
         }
         String (char* copy){
-            lenght = 1;
-            data = new char[2];
-            data[0] = *copy;
-            data[1] = '\0';
+			int i;
+			lenght = 0;
+			while (*(copy + lenght) != '\0'){
+				lenght = lenght + 1;
+			}
+			data = new char[lenght+1];
+			for (i = 0; i < lenght + 1; i++){
+				data[i] = copy[i];
+			}
+		//	printf("made char*\n");
         }
         String (String& copy)
         {
-            data = copy.data;
+			int i;
             lenght = copy.lenght;
+			data = new char[lenght + 1];
+			for (i = 0; i < lenght + 1; i++){
+				data[i] = copy.data[i];
+			}
+		//	printf("copy\n");
         }
         ~String()
         {
-            delete data;
+            delete[] data;
+		//	printf("delete\n");
         }
         String& operator=(String &orig)
         {
-            data = orig.data;
+			int i;
             lenght = orig.lenght;
+			data = new char[lenght + 1];
+			for (i = 0; i < lenght + 1; i++){
+				data[i] = orig.data[i];
+			}
+		//	printf("=\n");
             return *this;
         }
         operator const char *() const {
+		//	printf("change\n");
             return (const char *)data;
         }
 };
 int main()
 {
-    char *copy;
-    copy = new char[10];
-    char *data;
-    String a;
-    char b;
-    const char *god;
-    copy[2] = b;
-    copy = data;
-    String f(copy);
-    String g(a);
-    delete a;
-    g = f;
-    god = f;
+	String e;
+	std::cout << e<< "\n";
+	char *copy = "1244";
+	const char * change;
+	String test(copy);
+	std::cout << copy << "\n";	
+	String copy1(test);
+	std::cout << copy1 << "\n";
+	String c(copy1);	
+	c = copy1;
+	std::cout << c << "\n";
+	change = copy1;
+	std::cout << change << "\n";
+	system("pause");
     return 0;
 }
